1de3b5edbc7dcb7926087237d876daca70901618
Saro Mazzotta <Saromazzotta@gmail.com>
2023-01-19 19:20:45 -0500

    added ninja add feature in controller

--- flask-mysql/crud/core_assignments/dojos_and_ninjas/flask_app/controllers/dojos.py
+++ flask-mysql/crud/core_assignments/dojos_and_ninjas/flask_app/controllers/dojos.py
@@ -1,6 +1,7 @@
 from flask_app import app
 from flask import render_template, redirect, request
 from flask_app.models.dojo import Dojo
+from flask_app.models.ninja import Ninja
 
 @app.route('/')
 def index():
@@ -28,4 +29,25 @@ def ninja_show(dojo_id):
 
 @app.route('/ninjas')
 def ninjas():
-    return render_template("ninjas.html")
+    dojos = Dojo.get_all()
+    return render_template("ninjas.html", dojos=dojos)
+
+@app.route('/ninja/add', methods=['POST'])
+def add_ninja():
+    data = {
+        "first_name" : request.form['first_name'],
+        "last_name" : request.form['last_name'],
+        "age" : request.form['age'],
+        "dojo_id" : request.form['dojo_id']
+    }
+
+    Ninja.save(data)
+    return redirect(f'/dojos/{user_id}')
+
+# @app.route('/ninja/<int:dojo_id>/update')
+# def update_ninja():
+#     pass
+
+# @app.route('/ninja/<int:dojo_id/delete')
+# def delete_ninja():
+#     pass
