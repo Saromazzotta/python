e473295420ca41b47d6243317a2863bc4371dc96
Saro Mazzotta <Saromazzotta@gmail.com>
2023-01-26 11:52:08 -0500

    implemented confirm password feature

--- flask-mysql/validation/login_and_registartion/app/controllers/users.py
+++ flask-mysql/validation/login_and_registartion/app/controllers/users.py
@@ -1,8 +1,8 @@
 from app import app
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
 from app.models.user import User
-from flask import render_template, redirect, request, session
-
-
+from flask import render_template, redirect, request, session, flash
 
 
 @app.route('/')
@@ -17,11 +17,55 @@ def register():
         #redirect to the route where the form is rendered.
         return redirect('/')
     #else no errors:
-    User.save(request.form)
-    return redirect("/dashboard")
 
-@app.route('/dashboard', methods=['POST'])
+    # validate the form here ...
+    # create the hash
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+    # put the pw_hash into the data dictionary
+    data = {
+        "first_name" : request.form['first_name'],
+        "last_name" : request.form['last_name'],
+        "email": request.form['email'],
+        "password": pw_hash,
+        "confirm_password" : pw_hash
+    }
+    # Call the save @classmethod on User
+    user_id = User.save(data)
+    # store user id into session
+    session['user_id'] = user_id
+    return redirect("/success")
+
+
+@app.route('/success')
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("success.html")
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not User.validate_user(request.form):
+        return redirect('/')
+
+    data = {"email": request.form["email"]}
+    user_in_db = User.get_by_email(data)
+    # user is not registered in the db
+    if not user_in_db:
+        flash("Invalid Email/Password")
+        return redirect("/")
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        # if we get False after checking the password
+        flash("Invalid Email/Password")
+        return redirect('/')
+    # if the passwords matched, we set the user_id into session
+    session['user_id'] = user_in_db.id
+    # never render on a post!!!
+    return redirect("/success")
+
+
+
+@app.route('/logout')
+def logout():
+    return redirect("/")
 
 
--- flask-mysql/validation/login_and_registartion/app/models/user.py
+++ flask-mysql/validation/login_and_registartion/app/models/user.py
@@ -1,6 +1,9 @@
+from app import app
 from app.config.mysqlconnection import connectToMySQL
-from flask import flash 
+from flask import flash
 import re
+from flask_bcrypt import Bcrypt 
+bcrypt = Bcrypt(app)  # we are creating an object called bcrypt, which is made by invoking the function Bcrypt with our app as an argument
 
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 class User:
@@ -10,23 +13,27 @@ class User:
         self.last_name = data['last_name']
         self.email = data['email']
         self.password = data['password']
+        self.password = data['confirm_password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
     @staticmethod
     def validate_registration(user):
         is_valid = True # we assume this is true
-        if len(user['first_name']) < 3:
-            flash("First name must be at least 3 characters.")
+        if len(user['first_name']) < 2:
+            flash("First name must be at least 2 characters.")
             is_valid = False
-        if len(user['last_name']) < 3:
-            flash("Last name must be at least 3 characters.")
+        if len(user['last_name']) < 2:
+            flash("Last name must be at least 2 characters.")
             is_valid = False
         if len(user['email']) < 3:
             flash("Email must be at least 3 characters.")
             is_valid = False
-        if len(user['password']) < 3:
-            flash("Password must be at least 3 characters.")
+        if len(user['password']) < 8:
+            flash("Password must be at least 8 characters.")
+            is_valid = False
+        if len(user['password']) != len(user['confirm_password']):
+            flash("Passwords must match.")
             is_valid = False
         return is_valid
     
@@ -42,14 +49,14 @@ class User:
     @classmethod
     def save(cls,data):
         query = """
-        INSERT INTO users (first_name, last_name, email, password)
-        VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s)
+        INSERT INTO users (first_name, last_name, email, password, confirm_password)
+        VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, %(confirm_password)s)
         """
         return connectToMySQL('users').query_db(query,data)
     
 
     @classmethod
-    def get_one(cls, data):
+    def get_by_email(cls, data):
         query = """
         SELECT 
             *
@@ -57,9 +64,11 @@ class User:
             users
 
         WHERE 
-            id = %(id)s;
+            email = %(emails)s;
         """
 
-        results = connectToMySQL('users').query_db(query, data)
+        result = connectToMySQL('users').query_db(query, data)
 
-        return cls(results[0]) if results else None
+        if len(result) < 1:
+            return False
+        return cls(result[0])
